f977a3887d0a89cc4d931a872051847f62cdbf4a
Mauro Alexander Gamboa Mendez <Mauro.Alexander.Gamboa.Mendez@ey.com>
2021-03-08 10:09:55 -0300

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { authRoutes } from "./routes/auth.routes";
 import config from "./config/config";
 import { protectedRoutes } from "./routes/protected.routes";
 import { tokenErrorHandling } from "./middlewares/tokenValidation";
+import cors from "cors";
 
 // config express
 const app = express();
 const PORT = config.server.port;
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 //routes
 app.use(authRoutes);
@@ -16,6 +18,7 @@ app.use(protectedRoutes);
 
 //middlewares
 app.use(tokenErrorHandling);
+
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
