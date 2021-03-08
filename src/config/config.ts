72a2b42bb5bbe265c5e6cd69e4bc8b2166df29d5
Mauro Alexander Gamboa Mendez <Mauro.Alexander.Gamboa.Mendez@ey.com>
2021-03-08 10:46:29 -0300

    Remove mongodb harcode uri

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export default {
-  jwtSecret: process.env.MONGODB_SECRET || `MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA2vOMbfP11bZDIrTRVH4fk6VOfvSx3XnDSfqgwcvX1f0fxyOp7xHhA6ZOv9cN726RuNB/g1nE3G5ugOS3f28N`,
+  jwtSecret: process.env.JWT_SECRET || `MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA2vOMbfP11bZDIrTRVH4fk6VOfvSx3XnDSfqgwcvX1f0fxyOp7xHhA6ZOv9cN726RuNB/g1nE3G5ugOS3f28N`,
   DB: {
-    URI: process.env.MONGODB_URI || "mongodb://cosmos-mongodb-shopping2021:J6PRt1bVoRHdssoxHQWOWPJkW1sLmUJ214CTTMIZOylFkCjWuqBfASECqIB2vzovJJA47MIQp8EIuTdWrqIL0A==@cosmos-mongodb-shopping2021.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cosmos-mongodb-shopping2021@",
+    URI: process.env.MONGODB_URI || "",
   },
   server: {
     port: process.env.PORT || 8000 
